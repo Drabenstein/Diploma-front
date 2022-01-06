e8f90dee464565eae5f7e80200e69570f67b4af6
W12woj34 <woj120@gmail.com>
2022-01-06 12:44:51 +0100

    add modules directory

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,52 +1,52 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomePageComponent } from './shared/home-page/home-page.component';
-import { NonExistingPageComponent } from './shared/non-existing-page/non-existing-page.component';
+import { HomePageComponent } from './modules/shared/home-page/home-page.component';
+import { NonExistingPageComponent } from './modules/shared/non-existing-page/non-existing-page.component';
 
 const routes: Routes = [
   {
     path: 'approval',
     loadChildren: () =>
-      import('./topic-approval/topic-approval.module').then(
+      import('./modules/topic-approval/topic-approval.module').then(
         (m) => m.TopicApprovalModule
       ),
   },
   {
     path: 'list',
     loadChildren: () =>
-      import('./topic-list/topic-list.module').then((m) => m.TopicListModule),
+      import('./modules/topic-list/topic-list.module').then((m) => m.TopicListModule),
   },
   {
     path: 'review-assigments',
     loadChildren: () =>
-      import('./topic-review-assigment/topic-review-assigment.module').then(
+      import('./modules/topic-review-assigment/topic-review-assigment.module').then(
         (m) => m.TopicReviewAssigmentModule
       ),
   },
   {
     path: 'reviewer',
     loadChildren: () =>
-      import('./topic-reviewer/topic-reviewer.module').then(
+      import('./modules/topic-reviewer/topic-reviewer.module').then(
         (m) => m.TopicReviewerModule
       ),
   },
   {
     path: 'thesis',
     loadChildren: () =>
-      import('./topic-student/topic-student.module').then(
+      import('./modules/topic-student/topic-student.module').then(
         (m) => m.TopicStudentModule
       ),
   },
   {
     path: 'supervisor',
     loadChildren: () =>
-      import('./topic-supervisor/topic-supervisor.module').then(
+      import('./modules/topic-supervisor/topic-supervisor.module').then(
         (m) => m.TopicSupervisorModule
       ),
   },
   {
     path: 'user',
-    loadChildren: () => import('./user/user.module').then((m) => m.UserModule),
+    loadChildren: () => import('./modules/user/user.module').then((m) => m.UserModule),
   },
   { path: '404', component: NonExistingPageComponent },
   { path: '', component: HomePageComponent },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SharedModule } from './shared/shared.module';
-import { TopicApprovalModule } from './topic-approval/topic-approval.module';
-import { TopicListModule } from './topic-list/topic-list.module';
-import { TopicReviewAssigmentModule } from './topic-review-assigment/topic-review-assigment.module';
-import { TopicReviewerModule } from './topic-reviewer/topic-reviewer.module';
-import { TopicStudentModule } from './topic-student/topic-student.module';
-import { TopicSupervisorModule } from './topic-supervisor/topic-supervisor.module';
-import { UserModule } from './user/user.module';
+import { SharedModule } from './modules/shared/shared.module';
+import { TopicApprovalModule } from './modules/topic-approval/topic-approval.module';
+import { TopicListModule } from './modules/topic-list/topic-list.module';
+import { TopicReviewAssigmentModule } from './modules/topic-review-assigment/topic-review-assigment.module';
+import { TopicReviewerModule } from './modules/topic-reviewer/topic-reviewer.module';
+import { TopicStudentModule } from './modules/topic-student/topic-student.module';
+import { TopicSupervisorModule } from './modules/topic-supervisor/topic-supervisor.module';
+import { UserModule } from './modules/user/user.module';
 
 @NgModule({
   declarations: [
